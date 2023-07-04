76ce2b05b8d808725712f4feea7639c2dfe6bd73
Vivian Zitek <vivian.zitek@gmail.com>
2023-07-04 01:08:23 -0400

    Complete intermediate challenges.

--- 2023-07-01/fizzbuzz.f90
+++ 2023-07-01/fizzbuzz.f90
@@ -11,8 +11,21 @@ DO i=1, limit
 	IF (MODULO(i,3) .EQ. 0) THEN
 		WRITE(*,fmt="(a)",advance="no") "Fizz"
 	END IF
+
 	IF (MODULO(i,5) .EQ. 0) THEN
 		WRITE(*,fmt="(a)",advance="no") "Buzz"
+	END IF
+
+	IF (MODULO(i,7) .EQ. 0) THEN
+		WRITE(*,fmt="(a)",advance="no") "Rizz"
+	END IF
+
+	IF (MODULO(i,11) .EQ. 0) THEN
+		WRITE(*,fmt="(a)",advance="no") "Jazz"
+	END IF
+
+	IF (MODULO(120,i) .EQ. 0) THEN
+		WRITE(*,fmt="(a)",advance="no") "Dizz"
 	END IF
 
 	PRINT*,""
