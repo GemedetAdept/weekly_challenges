a4874d7868d81b002e330b22ac18a9ddbe8c3e52
Vivian Zitek <vivian.zitek@gmail.com>
2023-07-04 01:02:28 -0400

    Finish easy difficulty.

--- 2023-07-01/fizzbuzz.f90
+++ 2023-07-01/fizzbuzz.f90
@@ -1,6 +1,21 @@
 PROGRAM fizzbuzz
 IMPLICIT NONE
 
+integer:: i, limit
 
+limit = 100
 
-END PROGRAM
+DO i=1, limit
+	WRITE(*,fmt="(i0,1x)",advance="no") i
+
+	IF (MODULO(i,3) .EQ. 0) THEN
+		WRITE(*,fmt="(a)",advance="no") "Fizz"
+	END IF
+	IF (MODULO(i,5) .EQ. 0) THEN
+		WRITE(*,fmt="(a)",advance="no") "Buzz"
+	END IF
+
+	PRINT*,""
+END DO
+
+END PROGRAM fizzbuzz
